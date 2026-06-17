1c8872fd12f429bd9a0a975abafe003309eaad9f
LucasPivovar <lucascaetano956@gmail.com>
2026-06-17 13:37:52 -0300

    feat: place new senior chameleon image on the Inglês 50+ modality cover card and revert inside hero

--- src/views/Modalidades/Ingles50Plus.vue
+++ src/views/Modalidades/Ingles50Plus.vue
@@ -129,7 +129,7 @@ function submitForm() {
           </a>
         </div>
         <div class="hero-image-container">
-          <img src="/assets/img/ingles-50-hero-chameleon.png" alt="Inglês 50+">
+          <img src="/assets/img/ingles-50-hero.png" alt="Inglês 50+">
         </div>
       </div>
     </section>
